8cbfda5d4a5572e6a7a24b076532f90492f2c07b
Mervg1 <37779022+Mervg1@users.noreply.github.com>
2018-04-06 10:18:22 -0500

    Update Personaje.java

--- Personaje.java
+++ Personaje.java
@@ -1,8 +1,28 @@
 public abstract class Personaje implements AccionesP
 {
 	private int ataque;
-	priavte int vida;
+	private int vida;
 	private int defensa;
 	
 	public abstract void atacar(Personaje personaje);
+	
+	public int getX()
+	{
+		return x;
+	}
+
+	public void setX(int x)
+	{
+		this.x = x;
+	}
+
+	public int getY()
+	{
+		return y;
+	}
+
+	public void setY(int y)
+	{
+		this.y = y;
+	}
 }
