72b4e52a49191f0992b486760f953dfb643f4e63
Mervg1 <37779022+Mervg1@users.noreply.github.com>
2018-04-06 16:00:00 -0500

    Update Mercy.java

--- Mercy.java
+++ Mercy.java
@@ -1,8 +1,5 @@
 public class Mercy extends Estudiante
 {
-	private int vida;
-	private int ataque;
-	private String nombre;
 
 	public Mercy(int vida, int ataque)
 	{
@@ -10,36 +7,10 @@ public class Mercy extends Estudiante
 		this.ataque = ataque;
 	}
 
-	public String getNombre()
-	{
-		nombre = Mercy;
-		return nombre;
-	}
-
 	public void atacar(Maestro maestro)
 	{
 		super(maestro);
 		maestro.getVida() = maestro.getVida() - ataque;
-	}
-
-	public int getVida()
-	{
-		return vida;
-	}
-
-	public void setVida(int vida)
-	{
-		this.vida = vida;
-	}
-
-	public int getAtaque()
-	{
-		return ataque;
-	}
-
-	public void setAtaque(int vida)
-	{
-		this.ataque = ataque;
 	}
 	
 }
