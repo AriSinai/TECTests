8170d9da801257c3c099735e47a4962b44927531
Mervg1 <37779022+Mervg1@users.noreply.github.com>
2018-04-06 12:35:58 -0500

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,20 @@
+public class Item
+{
+  private String item1;
+  private int valorItem;
+  
+  public Item()
+  {
+    
+  }
+  
+  public String getItem()
+  {
+    return item;
+  }
+  
+  public int getValorItem()
+  {
+    retrun valorItem;
+  }
+   
