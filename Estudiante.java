abe1ee5ad6393f14a40579d32a2ee19f0fab7b89
Mervg1 <37779022+Mervg1@users.noreply.github.com>
2018-04-06 11:42:56 -0500

    Update Estudiante.java

--- Estudiante.java
+++ Estudiante.java
@@ -1,33 +1,13 @@
 public class Estudiante extends Personaje
 {
-	private int x;
-	private int y;
+
 	private Maestro maestro;
 
-	public Estudiante(int vida, int ataque, int defensa)
+	public Estudiante(int vida, int ataque, int defensa, int x, int y)
 	{
-		super(vida,ataque,defensa);
+		super(vida,ataque,defensa,x,y);
 	}
 	
-	public int getX()
-	{
-		return x;
-	}
 
-	public void setX(int x)
-	{
-		this.x = x;
-	}
-
-	public int getY()
-	{
-		return y;
-	}
-
-	public void setY(int y)
-	{
-		this.y = y;
-	}
-	
 
 }
