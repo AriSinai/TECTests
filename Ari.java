2ecfaa0335314929fdc59b359b470dd74fe59269
Mervg1 <37779022+Mervg1@users.noreply.github.com>
2018-04-05 23:16:55 -0500

    Update Ari.java

--- Ari.java
+++ Ari.java
@@ -1,20 +1,5 @@
 public class Ari extends Estudiante
 {
-	private int vida;
-	private int ataque;
-	private String nombre;
-
-	public Ari(int vida, int ataque)
-	{
-		this.vida = vida;
-		this.ataque = ataque;
-	}
-
-	public String getNombre()
-	{
-		nombre = Ari;
-		return nombre;
-	}
 
 	public void atacar(Maestro maestro)
 	{
@@ -22,24 +7,6 @@ public class Ari extends Estudiante
 		maestro.getVida() = maestro.getVida() - ataque;
 	}
 
-	public int getVida()
-	{
-		return vida;
-	}
-
-	public void setVida(int vida)
-	{
-		this.vida = vida;
-	}
-
-	public int getAtaque()
-	{
-		return ataque;
-	}
-
-	public void setAtaque(int vida)
-	{
-		this.ataque = ataque;
-	}
+	
 	
 }
