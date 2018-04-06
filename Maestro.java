3091344b681812b827a495550aebf4733c98b414
Mervg1 <37779022+Mervg1@users.noreply.github.com>
2018-04-06 12:14:16 -0500

    Update Maestro.java

--- Maestro.java
+++ Maestro.java
@@ -1,6 +1,18 @@
 public class Maestro extends Personaje
 {
 	private Estudiante estudiante;
+	
+	public Maestro(int vida, int ataque)
+	{
+		this.vida = vida;
+		this.ataque = ataque;
+	}
+	
+	
+	public void atacar(Personaje estudiante)
+	{
+		estudiante.setVida(estudiante.getVida()-get.ataque());
+	}
 
 	
 
