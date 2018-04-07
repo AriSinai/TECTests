8c5ecf8a30177a3f5448ad20224ea461fb9f8d94
Mervg1 <37779022+Mervg1@users.noreply.github.com>
2018-04-06 22:38:03 -0500

    Update Maestro.java

--- Maestro.java
+++ Maestro.java
@@ -1,11 +1,27 @@
 public class Maestro extends Personaje
 {
+	private int x,y;
 	private Estudiante estudiante;
 	
 	public Maestro(int vida, int ataque)
 	{
 		this.vida = vida;
 		this.ataque = ataque;
+	}
+	
+	public void setPosX(int posX){
+		this.posX=posX;
+	}
+
+	public int getPosX(){
+		return posX;
+	}
+	public void setPosY(int posY){
+		this.posY=posY;
+	}
+
+	public int getPosY(){
+		return posY;
 	}
 	
 	
