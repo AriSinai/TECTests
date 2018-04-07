778f4eb343480690215774f5377e6a614e488a12
Mervg1 <37779022+Mervg1@users.noreply.github.com>
2018-04-06 22:04:20 -0500

    Update CaptainLoop.java

--- CaptainLoop.java
+++ CaptainLoop.java
@@ -1,8 +1,40 @@
-public class CaptainLoop extends Maestro{
+public class CaptainLoop extends Maestro
+{
+	private Pregunta[] preguntas;
+	private Respuesta[] respuestas;
+	private int index;
+	
 	public CaptainLoop(int vida, int ataque)
 	{
 		super(vida, ataque);
 	}
+	
+	public void setIndex(int index)
+	{
+		this.index = index;
+	}
+	
+	public void preguntasProgra(Pregunta[] preguntas, int index)
+	{
+		preguntas.getPreguntasProgra[index];
+	}
+	
+	public void respuestasProgra0(Respuesta[] respuesta, int index)
+	{
+		respuestas.getRespuestasProgra0[index];
+	}
+	
+	public void respuestasProgra1(Respuesta[] respuesta, int index)
+	{
+		respuestas.getRespuestasProgra1[index];
+	}
+	
+	public void respuestasProgra2(Respuesta[] respuesta, int index)
+	{
+		respuestas.getRespuestasProgra2[index];
+	}
+	
+	
 	
 	
 }
