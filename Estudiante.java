b0be833810d5add87b74e401d5ffa037ad0fc45c
AriSinai <32492340+AriSinai@users.noreply.github.com>
2018-04-06 22:11:06 -0500

    Update Estudiante.java

--- Estudiante.java
+++ Estudiante.java
@@ -1,16 +1,78 @@
 public class Estudiante extends Personaje
 {
 
+	private Item[] items;
+	private int posX;
+	private int posY;
+	private String nombre;
 
-	public Estudiante(int ataque, int defensa,int vida, String nombre)
+	public Estudiante(int ataque, int defensa,int vida)
 	{
-		super(ataque,defensa,vida,nombre);
+		super(ataque,defensa,vida);
+		ataque = 15;
+		defensa = 60;
+		vida=100;
+		this.nombre=nombre;
+	}
+	public void setNombre (String nombre)
+	{
+		this.nombre = nombre;
 	}
 	
-	public void atacar(Personaje estudiante)
+	public String getNombre()
 	{
-		estudiante.setVida(estudiante.getVida()-get.ataque());
+		return nombre;
 	}
+	
+	public void addItem(Item item,int index){
+		items[index]=item;
+	}
+	public void removeItem(int index){
+		items[index]=null;
+	}
+	public Item[] getItems(){
+		return items;
+	}
+	
+	public void setPosX(int posX){
+		this.posX=posX;
+	}
+
+	public int getPosX(){
+		return posX;
+	}
+	public void setPosY(int posY){
+		this.posY=posY;
+	}
+
+	public int getPosY(){
+		return posY;
+	}
+	
+	
+	public int aumentarVida( Estudiante estudiante){
+		personaje.setVida(personaje.getVida()+50);
+	
+	}
+	public int aumentarDefensa(Estudiante estudiante){
+		personaje.setDefensa(personaje.getDefensa()+15);
+	
+	}
+	public int aumentarAtaque(Estudiante estudiante){
+		personaje.setAtaque(personaje.getAtaque()+10);
+	
+	}
+	
+	public void atacar (Personaje p){
+
+		Maestro m= (Maestro) p;
+
+		m.vida=m.vida - ataque;
+	}
+	
+	
+	
+	
 	
 
 
