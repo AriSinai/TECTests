c700d423aa4ed921c5f0260e69b2a9d076da7f98
Mervg1 <37779022+Mervg1@users.noreply.github.com>
2018-04-06 21:27:29 -0500

    Create Respuesta.java

---
+++ Respuesta.java
@@ -0,0 +1,62 @@
+public class Respuesta
+{
+	  private String[] respuestasMate0,respuestasMate1,respuestasMate2;
+  	private String[] respuestasHisto0,respuestasHisto1,respuestasHisto2;
+  	private String[] respuestasLite0,respuestasLite1,respuestasLite2;
+  	private String[] respuestasProgra0,respuestasProgra1,respuestasProgra2;
+
+  	public void getRespuestasMate0(String[] respuestasMate0)
+  	{
+
+		respuestasMate0 = new respuestasMate0[3];
+		respuestasMate0[0] = "Una ecuación irracional";
+		respuestasMate0[1] = "Una ecuación canónica";
+		respuestasMate0[2] = "Es la suma-resta de un conjunto de monomios";
+	}
+
+	public void getRespuestasMate1(String[] respuestasMate1)
+  	{
+
+		respuestasMate1 = new respuestasMate1[3];
+		respuestasMate1[0] = "Es el número primo 11";
+		respuestasMate1[1] = "Es un número trascendente que viene dado por el cociente entre la longitud de la circunferencia y su diámetro";
+		respuestasMate1[2] = "Es una suma de vectores";
+	}
+
+	public void getRespuestasMate2(String[] respuestasMate2)
+  	{
+
+		respuestasMate2 = new respuestasMate2[3];
+		respuestasMate2[0] = "5 multiplicado por pi";
+		respuestasMate2[1] = "2 veces el radio";
+		respuestasMate2[2] = "Ninguna de las anteriores";
+	}
+
+	public void getRespuestasHisto0(String[] respuestasHisto0)
+  	{
+
+		respuestasHisto0 = new respuestasHisto0[3];
+		respuestasHisto0[0] = "Olmeca";
+		respuestasHisto0[1] = "Azteca";
+		respuestasHisto0[2] = "Mexica";
+	}
+
+	public void getRespuestasHisto1(String[] respuestasHisto1)
+  	{
+
+		respuestasHisto1 = new respuestasHisto1[3];
+		respuestasHisto1[0] = "Teotihuacan";
+		respuestasHisto1[1] = "Tenochtitlan";
+		respuestasHisto1[2] = "Tepoztlán";
+	}
+
+	public void getRespuestasHisto2(String[] respuestasHisto2)
+  	{
+
+		respuestasHisto2 = new respuestasHisto2[3];
+		respuestasHisto2[0] = "Francia";
+		respuestasHisto2[1] = "Austria";
+		respuestasHisto2[2] = "España";
+	}
+
+}
