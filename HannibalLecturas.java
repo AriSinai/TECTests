4bcf44f9f493290d27a11122c842afa316567f3f
ferreyesc16 <37779027+ferreyesc16@users.noreply.github.com>
2018-04-06 22:06:38 -0500

    Update HannibalLecturas.java

--- HannibalLecturas.java
+++ HannibalLecturas.java
@@ -5,4 +5,27 @@ public class HannibalLecturas extends Maestro
 		super(vida);
 		super(ataque);
 	}
+	
+
+	public void setIndex(int index){
+	this.index= index;
+	}
+	
+	public void preguntasLite(Pregunta[] preguntas, int index){
+	
+	preguntas.getPreguntasLite[index];
+        }
+	
+	public void respuestasLite0(Respuesta[] respuestas, int index){
+	respuestas.getRespuestasLite0[index];
+	}
+	
+	public void respuestasLite1(Respuesta[] respuestas, int index){
+	respuestas.getRespuestasLite1[index];
+	}
+	
+	public void respuestasLite2(Respuesta[] respuestas, int index){
+	respuestas.getRespuestasLite2[index];
+	}
+	
 }
