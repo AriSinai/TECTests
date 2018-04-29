4ec8d538d66526119d920ca8c5e9b57a5c6d8914
Mervg1 <37779022+Mervg1@users.noreply.github.com>
2018-04-29 16:40:16 -0500

    Update Item.java

--- Item.java
+++ Item.java
@@ -14,6 +14,7 @@ public class Item
 	public ImageIcon removeImagen(){
 	}
 	
+	//En la ventana determinamos el nombre del item y llamamos al metodo que nos importe (platano,100)
 	public void itemVida(String nombre,int aumento){
 		estudiante.setVida(estudiante.getVida()+aumento);
 	}
