637c72d1b93d6a61642a274cf6eb071189a7cb8f
ferreyesc16 <37779027+ferreyesc16@users.noreply.github.com>
2018-04-30 19:31:02 -0500

    Update Campus.java

--- Campus.java
+++ Campus.java
@@ -1,8 +1,18 @@
 public class Campus{
 	
-	public Casilla [][] casillas;
+	private Casilla [][] casillas;
 
 	public Campus(int rows, int cols){
 		casillas= new Casilla[rows][cols];
 	}
+	
+	
+	public void setCasillas(Casilla[][] casillas){
+		this.casillas=casillas;
+		
+	public Casilla[][] getCasillas(){
+		return casillas;
+	}
+	
+	
 }
