bb4f904db3a7a4d4d4e956c4a44e37525e4af9d6
ferreyesc16 <37779027+ferreyesc16@users.noreply.github.com>
2018-04-30 20:44:10 -0500

    Update Mercy.java

--- Ari/Mercy.java
+++ Ari/Mercy.java
@@ -1,5 +1,6 @@
 public class Mercy extends Estudiante{
 	private String ruta = "Mercy.png";
+	private int ataque = 60;
 
 	public Mercy (int ataque,int defensa, int vida){
 		super(ataque, defensa,vida);
@@ -14,6 +15,16 @@ public class Mercy extends Estudiante{
 	public String getRuta(){
 		return ruta;
 
+	}
+		
+	
+	public void setAtaque(int ataque){
+		this.ataque=ataque;
+	}
+	
+	public int getAtaque(){
+		return ataque;
+		
 	}
 
 }
