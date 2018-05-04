9c8f89ae06aecb4b342d57c1ba779bc467e03cdd
ferreyesc16 <37779027+ferreyesc16@users.noreply.github.com>
2018-05-04 04:32:13 -0500

    Update PorfirioGuiaz.java

--- Proyecto2/PorfirioGuiaz.java
+++ Proyecto2/PorfirioGuiaz.java
@@ -1,4 +1,5 @@
-public class PorfirioGuiaz extends Profesor{
+import java.io.Serializable;
+public class PorfirioGuiaz extends Profesor implemennts Serializable{
 	public PorfirioGuiaz (int ataque, int vida){
 		super(ataque,vida,"PorfirioGuiaz.jpeg","caraPorfirioGuiaz.jpeg","aprobadoPorfirio.jpeg","perdistePorfirio.jpeg");
 
