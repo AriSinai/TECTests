768af2ea7cc3de1202930d0516e81c3d1f8bd94c
ferreyesc16 <37779027+ferreyesc16@users.noreply.github.com>
2018-05-04 04:31:10 -0500

    Update HannibalLecturas.java

--- Proyecto2/HannibalLecturas.java
+++ Proyecto2/HannibalLecturas.java
@@ -1,4 +1,5 @@
-public class HannibalLecturas extends Profesor{
+import java.io.Serializable;
+public class HannibalLecturas extends Profesor implements Serializable{
 	public HannibalLecturas (int ataque, int vida){
 		super(ataque,vida,"HannibalLecturas.jpeg","caraHannibalLecturas.jpeg","aprobadoHannibal.jpeg","perdisteHannibal.jpeg");
 
