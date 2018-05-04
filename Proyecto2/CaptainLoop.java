8dc3da589994097bac392e887839df6f08e6f1d1
ferreyesc16 <37779027+ferreyesc16@users.noreply.github.com>
2018-05-04 04:30:04 -0500

    Update CaptainLoop.java

--- Proyecto2/CaptainLoop.java
+++ Proyecto2/CaptainLoop.java
@@ -1,4 +1,5 @@
-public class CaptainLoop extends Profesor{
+import java.io.Serializable;
+public class CaptainLoop extends Profesor implements Serializable{
 	public CaptainLoop (int ataque, int vida){
 		super(ataque,vida,"CaptainLoop.jpeg","caraCaptainLoop.jpeg","aprobadoCaptain.jpeg","perdisteCaptain.jpeg");
 
